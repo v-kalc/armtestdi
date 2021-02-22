27fdd0d86d8096ed5e19cded66f0b98682b22bbc
v-kalc <59079664+v-kalc@users.noreply.github.com>
2021-02-22 18:07:58 +0530

    Added

---
+++ Source/Microsoft.Teams.Apps.DIConnect.Tests/PairUpFunction/SendPairUpNotificationFunctionTest.cs
@@ -0,0 +1,133 @@
+﻿// <copyright file="SendPairUpNotificationFunctionTest.cs" company="Microsoft">
+// Copyright (c) Microsoft. All rights reserved.
+// </copyright>
+
+namespace Microsoft.Teams.Apps.DIConnect.Tests.PairUpFunction
+{
+    using Moq;
+    using System;
+    using Microsoft.Extensions.Localization;
+    using Microsoft.Extensions.Logging;
+    using Microsoft.Teams.Apps.DIConnect.Common.Repositories.UserData;
+    using Microsoft.Teams.Apps.DIConnect.Common.Resources;
+    using Microsoft.Teams.Apps.DIConnect.Common.Services;
+    using Microsoft.Teams.Apps.DIConnect.Common.Services.Teams;
+    using Microsoft.Teams.Apps.DIConnect.Send.Func;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using Microsoft.Extensions.Caching.Memory;
+    using Microsoft.AspNetCore.Hosting;
+
+    /// <summary>
+    /// Class to test send pair up notification function.
+    /// </summary>
+    [TestClass]
+    public class SendPairUpNotificationFunctionTest
+    {
+        Mock<UserDataRepository> userDataRepository;
+        Mock<IMessageService> messageService;
+        Mock<IAppSettingsService> appSettingsService;
+        Mock<IStringLocalizer<Strings>> localizer;
+        Mock<IMemoryCache> memoryCache;
+        Mock<IHostingEnvironment> hostingEnvironment;
+
+        SendPairUpNotificationFunction sendPairUpNotificationFunction;
+
+        [TestInitialize]
+        public void SendPairUpNotificationFunctionTestSetup()
+        {
+            localizer = new Mock<IStringLocalizer<Strings>>();
+            var userDataLogger = new Mock<ILogger<UserDataRepository>>().Object;
+            userDataRepository = new Mock<UserDataRepository>(userDataLogger, TestData.repositoryOptions);
+            messageService = new Mock<IMessageService>();
+            appSettingsService = new Mock<IAppSettingsService>();
+            memoryCache = new Mock<IMemoryCache>();
+            hostingEnvironment = new Mock<IHostingEnvironment>();
+
+            sendPairUpNotificationFunction = new SendPairUpNotificationFunction(
+                messageService.Object,
+                userDataRepository.Object,
+                appSettingsService.Object,
+                hostingEnvironment.Object,
+                memoryCache.Object,
+                localizer.Object);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void SendPairUpNotificationFunction_ThrowsMessageServiceArgumentNullException()
+        {
+            new SendPairUpNotificationFunction(
+                null,
+                userDataRepository.Object,
+                appSettingsService.Object,
+                hostingEnvironment.Object,
+                memoryCache.Object,
+                localizer.Object);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void SendPairUpNotificationFunction_ThrowsUserDataRepositoryArgumentNullException()
+        {
+            new SendPairUpNotificationFunction(
+                messageService.Object,
+                null,
+                appSettingsService.Object,
+                hostingEnvironment.Object,
+                memoryCache.Object,
+                localizer.Object);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void SendPairUpNotificationFunction_ThrowsSettingServiceArgumentNullException()
+        {
+            new SendPairUpNotificationFunction(
+                messageService.Object,
+                userDataRepository.Object,
+                null,
+                hostingEnvironment.Object,
+                memoryCache.Object,
+                localizer.Object);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void SendPairUpNotificationFunction_ThrowsLocalizerArgumentNullException()
+        {
+            new SendPairUpNotificationFunction(
+                messageService.Object,
+                userDataRepository.Object,
+                appSettingsService.Object,
+                null,
+                memoryCache.Object,
+                localizer.Object);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void SendPairUpNotificationFunction_ThrowsMemoryCacheArgumentNullException()
+        {
+            new SendPairUpNotificationFunction(
+                messageService.Object,
+                userDataRepository.Object,
+                appSettingsService.Object,
+                hostingEnvironment.Object,
+                null,
+                localizer.Object);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void SendPairUpNotificationFunction_ThrowsHostingEnvirornmentArgumentNullException()
+        {
+            new SendPairUpNotificationFunction(
+                messageService.Object,
+                userDataRepository.Object,
+                appSettingsService.Object,
+                hostingEnvironment.Object,
+                memoryCache.Object,
+                null);
+        }
+    }
+}
---
+++ Source/Microsoft.Teams.Apps.DIConnect.Tests/PairUpFunction/TestData.cs
@@ -0,0 +1,27 @@
+﻿// <copyright file="TestData.cs" company="Microsoft">
+// Copyright (c) Microsoft. All rights reserved.
+// </copyright>
+
+namespace Microsoft.Teams.Apps.DIConnect.Tests.PairUpFunction
+{
+    using Microsoft.Extensions.Options;
+    using Microsoft.Teams.Apps.DIConnect.Common.Repositories;
+    using Microsoft.Teams.Apps.DIConnect.Common.Services.MessageQueues;
+   
+    /// <summary>
+    /// Class for test data.
+    /// </summary>
+    public static class TestData
+    {
+        public static readonly IOptions<MessageQueueOptions> messageQueueOptions = Options.Create(new MessageQueueOptions()
+        {
+            ServiceBusConnection = ""
+        });
+
+        public static readonly IOptions<RepositoryOptions> repositoryOptions = Options.Create(new RepositoryOptions()
+        {
+            StorageAccountConnectionString = "",
+            EnsureTableExists = false
+        });
+    }
+}
